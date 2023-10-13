c3a7a616383b88824db122687088e377c67c1c44
Abiyamm <abiyamm91@gmail.com>
2023-10-13 11:53:15 -0700

    task

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-'''The minimum operations coding challenge.
-'''
+"""In a text file, there is a single character H.
+ Your text editor can execute only two operations
+ in this file: Copy All and Paste. Given a number
+n, write a method that calculates the fewest number
+of operations needed to result in exactly n H
+characters in the file."""
+
 
 def minOperations(n: int) -> int:
     """calculates the fewest number of operations
