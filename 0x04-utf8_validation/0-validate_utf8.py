1b0f4d835fb161e80584f70a8c4732af3defc936
Abiyamm <abiyamm91@gmail.com>
2023-10-25 12:56:49 -0700

    tasks

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
-"""UTF-8 validation module.
+"""
+UTF-8 Validation
 """
 
 
 def validUTF8(data):
-    def get_num_of_bytes(num):
-        if num >> 3 == 0b11110:
-            return 4
-        elif num >> 4 == 0b1110:
-            return 3
-        elif num >> 5 == 0b110:
-            return 2
-        elif num >> 7 == 0:
-            return 1
-        return 0
+    num_bytes = 0
 
-    def check_continuation(index, data):
-        for i in range(index + 1, index + num_bytes):
-            if i >= len(data) or data[i] >> 6 != 0b10:
-                return False
-        return True
-
-    index = 0
-    while index < len(data):
-        num_bytes = get_num_of_bytes(data[index])
-
+    for d in data:
+        mask = 1 << 7
         if num_bytes == 0:
-            return False
+            while mask & d:
+                num_bytes += 1
+                mask = mask >> 1
 
-        if not check_continuation(index, data):
-            return False
+            if num_bytes == 0:
+                continue
 
-        index += num_bytes
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (d & 1 << 7 and not (d & 1 << 6)):
+                return False
 
-    return True
+        num_bytes -= 1
 
+    return num_bytes == 0
